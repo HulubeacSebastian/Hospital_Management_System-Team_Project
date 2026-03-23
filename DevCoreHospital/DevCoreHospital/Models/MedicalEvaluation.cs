0415e588153ffbf5d90783fc7ed12fa77c067c50
hidanvlad <vlad.hidan@stud.ubbcluj.ro>
2026-03-23 13:29:59 +0200

    Re-add: MedicalEvaluation model (had a conflict)

---
+++ DevCoreHospital/DevCoreHospital/Models/MedicalEvaluation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DevCoreHospital.Models
+{
+    public class MedicalEvaluation
+    {
+        public int EvaluationID { get; set; }
+
+        public int PatientID { get; set; }
+
+        public Doctor Evaluator { get; set; }
+
+        public string Symptoms { get; set; }
+
+        public string DiagnosisResult { get; set; }
+
+        public string MedsList { get; set; }
+
+        public string DoctorNotes { get; set; }
+
+        public DateTime EvaluationDate { get; set; }
+
+        public string Source { get; set; }
+
+        public MedicalEvaluation()
+        {
+            EvaluationDate = DateTime.Now;
+            Source = "PATIENT";
+        }
+    }
+
+    public class Doctor
+    {
+        public int DoctorID { get; set; }
+        public string Name { get; set; }
+        public string Specialization { get; set; }
+    }
+}
